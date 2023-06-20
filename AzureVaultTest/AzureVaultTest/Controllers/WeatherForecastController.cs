74bda3211c969ad0e0816432d5d3800469bc8ce6
AutodeskAP76 <adriano.parodi@autodesk.com>
2023-06-20 09:53:33 +0000

    First checkin
    First checkin

---
+++ AzureVaultTest/AzureVaultTest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureVaultTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            //  Read configuration values from configuration files. It is overwritten based on the configured files and configuration
+            /*  Order should be:
+             
+                -	Appsettings.json
+                -	Appsettings.[ENVIRONMENT].json based on the launchsettings.json --> environmentVariables.ASPNETCORE_ENVIRONMENT = [ENVIRONMENT]
+                -	Secret.json
+                -	Launchsettings.json
+
+            */
+
+            var configKey01_value = _configuration["appsettingsConfiguration:configKey01"];
+            var configSecret_value = "";
+
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
